7ebd9fa2862c62db6abff2b8f03ac60f56c6dc86
Hai Nam Dao <namdh2603@gmail.com>
2023-08-18 03:50:30 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,10 @@ jQuery(document).ready(function ($) {
   let bankNumber = "13310000015224";
   let qrType = "compact";
   var qr_image = `https://img.vietqr.io/image/${bankName}-${bankNumber}-${qrType}.png`;
-  $(".qr-code-img").attr("src", qr_image);
+
+    $(".qr-code").find("img").attr("src", qr_image);
+        $(".qr-code").find("a").attr("href", qr_image);
+
   $(".bank-list").on("click", ".bank-item", function () {
     let bankName = $(this).data("bank");
     if (bankName == "MOMO") {
@@ -12,6 +15,7 @@ jQuery(document).ready(function ($) {
       let bankNumber = $(this).data("stk");
       var qr_image = `https://img.vietqr.io/image/${bankName}-${bankNumber}-${qrType}.png`;
     }
-    $(".qr-code-img").attr("src", qr_image);
+    $(".qr-code").find("img").attr("src", qr_image);
+        $(".qr-code").find("a").attr("href", qr_image);
   });
 });
